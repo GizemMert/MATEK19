e6db2d35d23823f19e62b1812db2af65bdfc90c9
GizemMert <gizem.mert12@gmail.com>
2023-10-14 16:25:32 +0200

    input output torch size correction

--- Autoencoder.py
+++ Autoencoder.py
@@ -31,22 +31,22 @@ class Autoencoder(nn.Module):
         )
 
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(2048, 1024, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ConvTranspose2d(2048, 1024, kernel_size=5, stride=1, padding=2),
             nn.ReLU(),
 
-            nn.ConvTranspose2d(1024, 512, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ConvTranspose2d(1024, 512, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
 
-            nn.ConvTranspose2d(512, 256, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ConvTranspose2d(512, 256, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
 
-            nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1),
             nn.ReLU(),
 
-            nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1),
             nn.ReLU(),
 
-            nn.ConvTranspose2d(64, 3, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ConvTranspose2d(64, 3, kernel_size=3, stride=2, padding=1),
             nn.Tanh()
         )
 
