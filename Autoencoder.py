6540a87b472d28dfd134675f73522f6ed001e533
GizemMert <gizem.mert12@gmail.com>
2023-10-14 19:05:25 +0200

    autoencoder updated

--- Autoencoder.py
+++ Autoencoder.py
@@ -7,29 +7,28 @@ class Autoencoder(nn.Module):
 
         # Encoder layers
         self.encoder = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=3, stride=1),
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.Conv2d(64, 128, kernel_size=3, stride=2),
+            nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1),
             nn.BatchNorm2d(128),
             nn.ReLU(),
-            nn.Conv2d(128, 256, kernel_size=3, stride=1),
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.Conv2d(256, 512, kernel_size=3, stride=2),
             nn.BatchNorm2d(512),
             nn.ReLU(),
-            nn.Conv2d(512, 1024, kernel_size=3, stride=2),
+            nn.Conv2d(512, 1024, kernel_size=3, stride=2, padding=1),
             nn.BatchNorm2d(1024),
             nn.ReLU(),
-            nn.Conv2d(1024, 2048, kernel_size=3, stride=2),
+            nn.Conv2d(1024, 2048, kernel_size=3, stride=2, padding=1),
             nn.BatchNorm2d(2048),
             nn.ReLU()
         )
 
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(2048, 1024, kernel_size=3, stride=2, padding=1, output_padding=2),
-            # Adjusted output padding
+            nn.ConvTranspose2d(2048, 1024, kernel_size=3, stride=2, padding=1, output_padding=1),
             nn.ReLU(),
 
             nn.ConvTranspose2d(1024, 512, kernel_size=3, stride=2, padding=1, output_padding=1),
@@ -38,13 +37,10 @@ class Autoencoder(nn.Module):
             nn.ConvTranspose2d(512, 256, kernel_size=3, stride=2, padding=1, output_padding=1),
             nn.ReLU(),
 
-            nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1, output_padding=1),  # New layer
+            nn.ConvTranspose2d(256, 64, kernel_size=3, stride=2, padding=1, output_padding=1),
             nn.ReLU(),
 
-            nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1, output_padding=1),
-            nn.ReLU(),
-
-            nn.ConvTranspose2d(64, 3, kernel_size=3, stride=1),
+            nn.ConvTranspose2d(64, 3, kernel_size=3, stride=1, padding=1),
             nn.Tanh()
         )
 
