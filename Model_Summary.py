38695a16f61a4673da1266a659bbea758e4e961c
GizemMert <gizem.mert12@gmail.com>
2023-10-18 09:10:32 +0200

    updated

--- Model_Summary.py
+++ Model_Summary.py
@@ -1,14 +1,11 @@
 import torch
 from Model_Custom import CustomNetwork
+from torchsummary import summary
+from Autoencoder import Autoencoder
 
+model = Autoencoder()
 
-model = CustomNetwork(14)
+sample_input = torch.randn(128, 3, 224, 224)
+# Print the model summary
+summary(model, input_size=sample_input.shape[1:])
 
-# Create a random input tensor with the same dimensions as your expected input (e.g., batch size 128, 3 channels, 224x224)
-sample_input = torch.randn(128, 3, 224, 224)  # Adjust the batch size as needed
-
-# Pass the input through the model
-output = model(sample_input)
-
-# Print the size of the output tensor
-print("Output size:", output.size())
