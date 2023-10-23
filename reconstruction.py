36e2ac19f7479830a827086834c88d6ee971c723
GizemMert <gizem.mert12@gmail.com>
2023-10-23 16:10:47 +0200

    evaluation

--- reconstruction.py
+++ reconstruction.py
@@ -4,32 +4,39 @@ import numpy as np
 from PIL import Image
 from DataLoader import get_data_loaders
 from Autoencoder import Autoencoder
+from torchvision.utils import save_image
 
-
+batch_size = 64
 _, val_loader, _ = get_data_loaders('/lustre/groups/labs/marr/qscd01/datasets/191024_AML_Matek'
                                     '/train_val_test',
-                                    batch_size=1, num_workers=0)
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+                                    batch_size=batch_size, num_workers=0)
+
+unique_labels = set()
+for _, label in val_loader.dataset:
+    unique_labels.add(label)
+
+num_classes = len(unique_labels)
+
 model = Autoencoder()
 model.load_state_dict(torch.load('best_autoencoder_model.pth'))
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
-
 model.eval()
 
-save_folder = "outputsvsimages"
-os.makedirs(save_folder, exist_ok=True)
+output_folder = "ten_reconstruct"
+os.makedirs(output_folder, exist_ok=True)
+images_to_compare = []
 
 with torch.no_grad():
     for i, (images, _) in enumerate(val_loader):
         images = images.to(device)
         outputs = model(images)
 
-        images = (images * 255).cpu().detach().numpy().astype(np.uint8)
-        outputs = (outputs * 255).cpu().detach().numpy().astype(np.uint8)
-        concatenated_images = np.concatenate((images, outputs), axis=2)
+        if i < 10:
+            images = images * 255
+            outputs = outputs * 255
+            image = torch.cat([images[i], outputs[i]], dim=2).cpu()
+            images_to_compare.append(image)
 
-        concatenated_image = Image.fromarray(concatenated_images[0].transpose(1, 2, 0))
-        concatenated_image.save(os.path.join(save_folder, f'image_{i}.jpg'))
-
-        if i >= 10:
-            break
+for i in range(10):
+    save_image(images_to_compare[i], os.path.join(output_folder, f'reconstructed_{i}.png'), nrow=1)
