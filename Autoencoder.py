b9ba8af88bd89ed0edc24ef42a2a28f2de172eca
GizemMert <gizem.mert12@gmail.com>
2023-10-23 23:34:34 +0200

    model updated

--- Autoencoder.py
+++ Autoencoder.py
@@ -7,46 +7,46 @@ class Autoencoder(nn.Module):
 
         # Encoder layers
         self.encoder = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(3, 64, kernel_size=3, stride=1),
             nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(64, 128, kernel_size=3, stride=2),
             nn.BatchNorm2d(128),
             nn.ReLU(),
-            nn.MaxPool2d(2, 2),
-            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            # nn.MaxPool2d(2, 2),
+            nn.Conv2d(128, 256, kernel_size=3, stride=1),
             nn.BatchNorm2d(256),
             nn.ReLU(),
-            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(256, 512, kernel_size=2, stride=2),
             nn.BatchNorm2d(512),
             nn.ReLU(),
-            nn.MaxPool2d(2, 2),
-            nn.Conv2d(512, 1024, kernel_size=3, stride=2, padding=1),
+            # nn.MaxPool2d(2, 2),
+            nn.Conv2d(512, 1024, kernel_size=2, stride=2),
             nn.BatchNorm2d(1024),
             nn.ReLU(),
-            nn.Conv2d(1024, 2048, kernel_size=3, stride=2, padding=1),
+            nn.Conv2d(1024, 2048, kernel_size=1, stride=2),
             nn.BatchNorm2d(2048),
             nn.ReLU(),
-            nn.AdaptiveAvgPool2d((7, 7))
+            # nn.AdaptiveAvgPool2d((7, 7))
         )
 
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(2048, 1024, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ConvTranspose2d(2048, 1024, kernel_size=1, stride=2),
             nn.ReLU(),
 
-            nn.ConvTranspose2d(1024, 512, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2),
             nn.ReLU(),
 
-            nn.ConvTranspose2d(512, 256, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2),
             nn.ReLU(),
 
-            nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ConvTranspose2d(256, 128, kernel_size=3, stride=1),
             nn.ReLU(),
 
-            nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2),
             nn.ReLU(),
 
-            nn.ConvTranspose2d(64, 3, kernel_size=3, stride=1, padding=1),
+            nn.ConvTranspose2d(64, 3, kernel_size=4, stride=1),
             nn.Sigmoid()
         )
 
