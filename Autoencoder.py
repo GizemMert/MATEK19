282e9bf2c35a2d7a635657cbb96f2399a83515d4
GizemMert <gizem.mert12@gmail.com>
2023-10-25 17:31:21 +0200

    model updated

--- Autoencoder.py
+++ Autoencoder.py
@@ -18,37 +18,37 @@ class Autoencoder(nn.Module):
             nn.Conv2d(256, 128, kernel_size=2, stride=2),
             nn.BatchNorm2d(128),
             nn.ReLU(),
-            nn.Conv2d(128, 64, kernel_size=2, stride=2),
+            nn.Conv2d(128, 64, kernel_size=2, stride=1),
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.Conv2d(64, 20, kernel_size=2, stride=1),
             nn.BatchNorm2d(20),
             nn.ReLU(),
-            # nn.Flatten()
+            nn.Flatten(),
+            # nn.Linear(15680, 1000),
+            # nn.ReLU(),
         )
 
         self.decoder = nn.Sequential(
-            # nn.Unflatten(1, (20, 14, 14)),
+            # nn.Linear(1000, 15680),
+            nn.Unflatten(1, (20, 28, 28)),
 
-            nn.ConvTranspose2d(20, 64, kernel_size=3, stride=2),
+            nn.ConvTranspose2d(20, 64, kernel_size=2, stride=1),
             nn.ReLU(),
 
-            nn.ConvTranspose2d(64, 128, kernel_size=3, stride=1),
+            nn.ConvTranspose2d(64, 128, kernel_size=2, stride=1),
             nn.ReLU(),
 
-            nn.ConvTranspose2d(128, 256, kernel_size=3, stride=2),
+            nn.ConvTranspose2d(128, 256, kernel_size=2, stride=2),
             nn.ReLU(),
 
-            nn.ConvTranspose2d(256, 200, kernel_size=2, stride=1),
+            nn.ConvTranspose2d(256, 200, kernel_size=3, stride=1),
             nn.ReLU(),
 
-            nn.ConvTranspose2d(200, 150, kernel_size=2, stride=2),
+            nn.ConvTranspose2d(200, 150, kernel_size=4, stride=2),
             nn.ReLU(),
 
-            nn.ConvTranspose2d(150, 64, kernel_size=1, stride=1),
-            nn.ReLU(),
-
-            nn.ConvTranspose2d(64, 3, kernel_size=1, stride=1),
+            nn.ConvTranspose2d(150, 3, kernel_size=3, stride=1),
             nn.Sigmoid()
         )
 
