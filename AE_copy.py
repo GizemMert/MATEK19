b394611ec90faa5daaded9f1a0abf72b34e3518e
GizemMert <gizem.mert12@gmail.com>
2023-10-26 15:38:53 +0200

    updated

--- AE_copy.py
+++ AE_copy.py
@@ -6,23 +6,23 @@ class Autoencoder1(nn.Module):
         super(Autoencoder1, self).__init__()
 
         self.encoder = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=3, stride=1),
+            nn.Conv2d(3, 64, kernel_size=1, stride=1),
             nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.Conv2d(64, 128, kernel_size=3, stride=2),
+            nn.Conv2d(64, 128, kernel_size=1, stride=2),
             nn.BatchNorm2d(128),
             nn.ReLU(),
-            nn.Conv2d(128, 256, kernel_size=3, stride=1),
+            nn.Conv2d(128, 256, kernel_size=1, stride=1),
             nn.BatchNorm2d(256),
             nn.ReLU(),
-            nn.Conv2d(256, 128, kernel_size=2, stride=2),
-            nn.BatchNorm2d(128),
+            nn.Conv2d(256, 100, kernel_size=1, stride=2),
+            nn.BatchNorm2d(100),
             nn.ReLU(),
-            nn.Conv2d(128, 64, kernel_size=2, stride=1),
-            nn.BatchNorm2d(64),
+            nn.Conv2d(100, 30, kernel_size=1, stride=1),
+            nn.BatchNorm2d(30),
             nn.ReLU(),
-            nn.Conv2d(64, 10, kernel_size=2, stride=1),
-            nn.BatchNorm2d(10),
+            nn.Conv2d(30, 5, kernel_size=1, stride=1),
+            nn.BatchNorm2d(5),
             nn.ReLU(),
             # nn.Flatten(),
             # nn.Linear(7840, 1000)
@@ -32,22 +32,22 @@ class Autoencoder1(nn.Module):
             # nn.Linear(1000, 7840),
 
             # nn.Unflatten(1, (10, 28, 28)),
-            nn.ConvTranspose2d(10, 64, kernel_size=2, stride=1),
+            nn.ConvTranspose2d(5, 30, kernel_size=1, stride=1),
             nn.ReLU(),
 
-            nn.ConvTranspose2d(64, 128, kernel_size=2, stride=1),
+            nn.ConvTranspose2d(30, 100, kernel_size=1, stride=1),
             nn.ReLU(),
 
-            nn.ConvTranspose2d(128, 256, kernel_size=2, stride=2),
+            nn.ConvTranspose2d(100, 256, kernel_size=1, stride=2),
             nn.ReLU(),
 
-            nn.ConvTranspose2d(256, 128, kernel_size=3, stride=1),
+            nn.ConvTranspose2d(256, 200, kernel_size=2, stride=1),
             nn.ReLU(),
 
-            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2),
+            nn.ConvTranspose2d(200, 150, kernel_size=2, stride=2),
             nn.ReLU(),
 
-            nn.ConvTranspose2d(64, 3, kernel_size=3, stride=1),
+            nn.ConvTranspose2d(150, 3, kernel_size=1, stride=1),
             nn.Sigmoid()
         )
 
--- reconstruction.py
+++ reconstruction.py
@@ -23,7 +23,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 model.eval()
 
-output_folder = "102828_reconstruct_2"
+output_folder = "5x32x32_reconstruct"
 os.makedirs(output_folder, exist_ok=True)
 images_to_compare = []
 
